19f312bee30d46aaef7ac65f78f3431d0c4fe358
igatai <taiti0614@gmail.com>
2020-04-29 01:03:10 +0900

    commit

--- app/models/production.rb
+++ app/models/production.rb
@@ -1,3 +1,15 @@
 class Production < ApplicationRecord
-  belongs_to :House
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      production = find_by(id: row["id"]) || new
+      production.attributes = row.to_hash.slice(*updatable_attributes)
+      production.save
+    end
+  end
+  def self.updatable_attributes
+    ["id", "Label", "House_id", "Year", "Month", "Temperature", "Daylight", "EnergyProduction" ]
+  end
+
+  belongs_to :house
 end
